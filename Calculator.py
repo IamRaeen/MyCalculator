e86412e040807ff55cd2b35699441a2222916461
IamRaeen <iqbal.raeen@gmail.com>
2018-12-12 00:30:57 +0530

    New Update 1.1

---
+++ Calculator.py
@@ -0,0 +1,20 @@
+
+
+a = int(input('Enter a number : '))
+b = input('Enter the sign : ')
+c = int(input('Enter another number number : '))
+
+
+if b == "+":
+    print(a+c)
+elif b == "-":
+    print(a - c)
+elif b == "*":
+    print(a * c)
+elif b == "/":
+    print(a / c)
+else:
+    print('Please Choose the correct Operator')
+
+
+#This Was wrtten by Iqbal
